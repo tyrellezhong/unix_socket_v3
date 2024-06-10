b1af5e9c05b27b3f6c34bab865934913fcacface
taylorzhong <taylorzhong@tencent.com>
2024-06-10 17:25:34 +0800

    feat： 编译通过配置

--- libfree/inet_ntop.c
+++ libfree/inet_ntop.c
@@ -52,24 +52,20 @@ static const char *inet_ntop6(const u_char *src, char *dst, size_t size);
  * author:
  *	Paul Vixie, 1996.
  */
-const char *
-inet_ntop(af, src, dst, size)
-	int af;
-	const void *src;
-	char *dst;
-	size_t size;
-{
-	switch (af) {
-	case AF_INET:
-		return (inet_ntop4(src, dst, size));
-	case AF_INET6:
-		return (inet_ntop6(src, dst, size));
-	default:
-		errno = EAFNOSUPPORT;
-		return (NULL);
-	}
-	/* NOTREACHED */
-}
+// const char *
+// inet_ntop(int af, const void *src, char *dst, size_t size)
+// {
+// 	switch (af) {
+// 	case AF_INET:
+// 		return (inet_ntop4(src, dst, size));
+// 	case AF_INET6:
+// 		return (inet_ntop6(src, dst, size));
+// 	default:
+// 		errno = EAFNOSUPPORT;
+// 		return (NULL);
+// 	}
+// 	/* NOTREACHED */
+// }
 
 /* const char *
  * inet_ntop4(src, dst, size)
